ce09984f75fe8cdc9fe2c035d05ce3740213b088
Antastsy7 <664692054@qq.com>
2019-11-14 17:04:48 -0500

    Emissive Rework

---
+++ Source/Bat/Emissive_Basic.cpp
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Emissive_Basic.h"
+#include "Engine/Engine.h"
+#include "ConstructorHelpers.h"
+
+// Sets default values
+AEmissive_Basic::AEmissive_Basic()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CubeRoot = CreateAbstractDefaultSubobject<USceneComponent>(TEXT("CubeRoot"));
+	RootComponent = CubeRoot;
+	
+	CubeMesh = CreateAbstractDefaultSubobject<UStaticMeshComponent>(TEXT("CubeMesh"));
+	CubeMesh->AttachToComponent(CubeRoot, FAttachmentTransformRules::SnapToTargetNotIncludingScale);
+
+	Collision = CreateAbstractDefaultSubobject<UBoxComponent>(TEXT("Collision"));
+	Collision->SetGenerateOverlapEvents(true);
+	Collision->SetWorldScale3D(FVector(2.0f, 2.0f, 2.0f));
+	Collision->OnComponentBeginOverlap.AddDynamic(this, &AEmissive_Basic::OnSoundWaveRecieve);
+	Collision->AttachToComponent(CubeRoot, FAttachmentTransformRules::SnapToTargetNotIncludingScale);
+
+	strength = 1.0f;
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> CylinderAsset(TEXT("/Game/Geometry/Emssive.Emssive"));
+	if (CylinderAsset.Succeeded())
+	{
+		CubeMesh->SetStaticMesh(CylinderAsset.Object);
+	}
+
+	
+
+
+}
+
+// Called when the game starts or when spawned
+void AEmissive_Basic::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AEmissive_Basic::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (strength >= 0) {
+		strength -= DeltaTime;
+	}
+
+	CubeMesh->SetScalarParameterValueOnMaterials("Strength", strength);
+	
+}
+
+void AEmissive_Basic::OnSoundWaveRecieve(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	strength += 1;
+	if (GEngine) {
+		GEngine->AddOnScreenDebugMessage(-1, 1.5f, FColor::White, TEXT("1"), false);
+	}
+}
+
---
+++ Source/Bat/Emissive_Basic.h
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Emissive_Basic.generated.h"
+
+UCLASS()
+class BAT_API AEmissive_Basic : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AEmissive_Basic();
+
+protected:
+
+	void BeginPlay();
+
+public:	
+
+	UPROPERTY(EditAnyWhere)
+		USceneComponent* CubeRoot;
+
+	UPROPERTY(EditAnyWhere)
+		UStaticMeshComponent* CubeMesh;
+
+	UPROPERTY(EditAnyWhere)
+		UBoxComponent* Collision;
+	
+	void Tick(float DeltaTime);
+
+	UFUNCTION()
+	void OnSoundWaveRecieve(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+
+
+
+private:
+	float strength;
+
+};
