453ad899805cf677079506303b9d2d1d4353f5bc
Chengcheng Ding <ding.che@husky.neu.edu>
2020-01-21 15:32:51 -0500

    master draft level
    Co-Authored-By: antastsy7 <664692054@qq.com>

---
+++ Source/Bat/WildHunter.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WildHunter.h"
+
+// Sets default values
+AWildHunter::AWildHunter()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AWildHunter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AWildHunter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
