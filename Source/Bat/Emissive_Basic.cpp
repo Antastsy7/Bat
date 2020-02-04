796cb8857c7a6321e1d082401bdc5b44f8f72b17
Chengcheng Ding <ding.che@husky.neu.edu>
2020-02-04 16:08:02 -0500

    White
    Co-Authored-By: antastsy7 <664692054@qq.com>

--- Source/Bat/Emissive_Basic.cpp
+++ Source/Bat/Emissive_Basic.cpp
@@ -65,7 +65,7 @@ void AEmissive_Basic::Tick(float DeltaTime)
 
 void AEmissive_Basic::OnSoundWaveRecieve(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (OtherActor->ActorHasTag(ActorTag) || OverlappedComp->ComponentHasTag(ComponentTag)) {
+	if (OtherActor->ActorHasTag(ActorTag) || OtherComp->ComponentHasTag(ComponentTag)) {
 		if (Activated) {
 			strength = strength + Addon;
 			if (strength > MaxStrength) {
