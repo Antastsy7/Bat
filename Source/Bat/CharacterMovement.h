ead02b4157d0ab600f1b63599f58243792ef3d39
Chengcheng Ding <ding.che@husky.neu.edu>
2020-02-08 20:20:21 -0500

    new bow&arrow

---
+++ Source/Bat/CharacterMovement.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "CharacterMovement.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BAT_API UCharacterMovement : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+	
+};
